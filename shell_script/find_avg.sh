805f2b39086ae13e37e82d429a1178b681db7199
lakshminarayana8522 <narayana8522@gmail.com>
2022-01-07 08:12:56 +0530

    day_1

--- shell_script/find_avg.sh
+++ shell_script/find_avg.sh
@@ -4,17 +4,12 @@ echo -n "Enter count of numbers: "
 read n
 echo "Enter numbers: "
 i=0
-while [ $i -lt $n ]
-do
-	read arr[$i]
-	i=`expr $i + 1`
-done
-i=0
 sum=0
 while [ $i -lt $n ]
 do
-	sum=`expr $sum + ${arr[$i]}`
+	read num
+	sum=`expr $sum + $num`
 	i=`expr $i + 1`
 done
 avg=`expr $sum / $n`
-echo "Average: "$avg
+echo "average: "$avg
